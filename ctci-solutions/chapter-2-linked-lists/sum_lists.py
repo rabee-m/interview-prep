f5b650f2037cf5616e5a980748120a0f69026ae4
Rabee <rabee@Talals-MacBook-Air.local>
2022-06-14 19:26:46 -0400

    add work for sum_lists.py

--- ctci-solutions/chapter-2-linked-lists/sum_lists.py
+++ ctci-solutions/chapter-2-linked-lists/sum_lists.py
@@ -36,3 +36,43 @@ def sum_lists_reverse(llist1, llist2):
     return sum_llist
 
 #Add forward sum implementation
+#e.g.) Adding 6 -> 1 -> 7 + 2 -> 9 -> 5 = 9 -> 1 -> 2 (617 + 295)
+
+
+#Things to do
+#Find the larger number and add zero padding to the front until lengths match
+#Store the previous node and add carryOvers directly to it
+#TODO: Not finished
+def sum_list_forward(llist1, llist2):
+    #Get length of list1 and list2
+    lenList1 = 0
+    currNode = llist1
+    while(llist1):
+        lenList1 += 1
+        llist1 = llist1.next
+    lenList2 = 0
+    currNode = llist2
+    while(llist2):
+        llist2 += 1
+        llist2 = llist2.next
+    
+    smallerList = llist2 if lenList1 > lenList2 else llist1
+    lengthDiff = lenList2 - lenList1
+    #Add zero padding to smaller list
+    # 1-> 2 -> 3
+    # 0-> 1-> 2-> 3
+    while(lengthDiff != 0):
+        zeroNode = LinkedList(0)
+        zeroNode.next = smallerList
+        smallerList = zeroNode
+        lengthDiff -= 1
+    
+    currNode1 = llist1
+    currNode2 = llist2
+    sumllist = LinkedList(0)
+    prevSumllistNode = sumllist
+    while (currNode1 is not None and currNode2 is not None):
+        sumVal = currNode1 + currNode2
+        carryOver = sumVal % 10
+        prevSumllistNode.value += carryOver
+
