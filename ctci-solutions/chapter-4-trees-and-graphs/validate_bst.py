357fe0e202b384828f3768c1a32df9586be6a388
Muhammad Rabee <muhammad.rabee02@gmail.com>
2022-07-02 22:48:43 -0400

    add validate_bst solution

--- ctci-solutions/chapter-4-trees-and-graphs/validate_bst.py
+++ ctci-solutions/chapter-4-trees-and-graphs/validate_bst.py
@@ -6,4 +6,21 @@ class Node:
         self.left = None
         self.right = None
         
+def validate_bst(root):
+    return validate_bst_helper(root, float("-inf"), float("inf"))
 
+def validate_bst_helper(node, left, right):
+    if node is None:
+        #base case
+        return True
+    
+    if not (node.value < right and node.value > left):
+        #check that current node is valid by making sure it is greater than right, andd less than left
+        return False
+    left_tree_valid = validate_bst_helper(node.left, left, node.value) #min value is still the same, but values can no longer be grater than node.value
+    right_tree_valid = validate_bst_helper(node.right, node.value, right)
+    
+    if left_tree_valid and right_tree_valid:
+        return True
+    else: return False
+    
